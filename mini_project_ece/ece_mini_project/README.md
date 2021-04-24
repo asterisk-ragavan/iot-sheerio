836871a4a7d779d275d6903df506cd3be8ab7ccf
Sakthi Ragavan <75389172+asterisk-ragavan@users.noreply.github.com>
2021-04-24 13:21:37 +0530

    Add files via upload

---
+++ mini_project_ece/ece_mini_project/README.md
@@ -0,0 +1,13 @@
+# ECE_mini_project
+
+This project is based on nodeMCU(ESP8266), this is it based wifi module which can communicate with serial USB or through wifi using this I have designed to control 4 led's from the phone, google assistant and the physical buttons
+
+this is my binary file which u can upload to node MCU using Arduino ide,
+#note 
+pins in Arduino ide is mapped with the Arduino UNO for the corresponding node MCU GPIO pins I have included the pin diagram and picture of my project down below
+
+# PIN_DIAGRAM
+![ESP8266-NodeMCU-kit-12-E-pinout-gpio-pin](https://user-images.githubusercontent.com/75389172/115951587-1c19ca00-a4ff-11eb-8b74-a68eade88404.png)
+
+# MY_PROJECT
+![IMG_20210424_131641_2](https://user-images.githubusercontent.com/75389172/115951661-829ee800-a4ff-11eb-8b3a-42ba54c22876.jpg)
---
+++ mini_project_ece/mini_project_ece.ino
@@ -0,0 +1,226 @@
+//#define BLYNK_PRINT Serial  
+#include <ESP8266WiFi.h>          
+#include <BlynkSimpleEsp8266.h> 
+
+// define the GPIO connected with Relays and switches
+#define RelayPin1 5  //D1
+#define RelayPin2 4  //D2
+#define RelayPin3 14 //D5
+#define RelayPin4 12 //D6
+
+#define SwitchPin1 10  //SD3
+#define SwitchPin2 0   //D3 
+#define SwitchPin3 13  //D7
+#define SwitchPin4 3   //RX
+
+#define wifiLed   16   //D0
+
+#define VPIN_BUTTON_1    V1 
+#define VPIN_BUTTON_2    V2
+#define VPIN_BUTTON_3    V3 
+#define VPIN_BUTTON_4    V4
+
+int toggleState_1 = 1; //Define integer to remember the toggle state for relay 1
+int toggleState_2 = 1; //Define integer to remember the toggle state for relay 2
+int toggleState_3 = 1; //Define integer to remember the toggle state for relay 3
+int toggleState_4 = 1; //Define integer to remember the toggle state for relay 4
+
+int wifiFlag = 0;
+
+#define AUTH "muIzC8yYeXB3Bt0udDjW0Z1hoGvFK2Wt"                 // You should get Auth Token in the Blynk App.  
+#define WIFI_SSID "Thanmozi"             //Enter Wifi Name
+#define WIFI_PASS "12345678"         //Enter wifi Password
+
+BlynkTimer timer;
+
+void relayOnOff(int relay){
+
+    switch(relay){
+      case 1: 
+             if(toggleState_1 == 1){
+              digitalWrite(RelayPin1, LOW); // turn on relay 1
+              toggleState_1 = 0;
+              Serial.println("Device1 ON");
+              }
+             else{
+              digitalWrite(RelayPin1, HIGH); // turn off relay 1
+              toggleState_1 = 1;
+              Serial.println("Device1 OFF");
+              }
+             delay(100);
+      break;
+      case 2: 
+             if(toggleState_2 == 1){
+              digitalWrite(RelayPin2, LOW); // turn on relay 2
+              toggleState_2 = 0;
+              Serial.println("Device2 ON");
+              }
+             else{
+              digitalWrite(RelayPin2, HIGH); // turn off relay 2
+              toggleState_2 = 1;
+              Serial.println("Device2 OFF");
+              }
+             delay(100);
+      break;
+      case 3: 
+             if(toggleState_3 == 1){
+              digitalWrite(RelayPin3, LOW); // turn on relay 3
+              toggleState_3 = 0;
+              Serial.println("Device3 ON");
+              }
+             else{
+              digitalWrite(RelayPin3, HIGH); // turn off relay 3
+              toggleState_3 = 1;
+              Serial.println("Device3 OFF");
+              }
+             delay(100);
+      break;
+      case 4: 
+             if(toggleState_4 == 1){
+              digitalWrite(RelayPin4, LOW); // turn on relay 4
+              toggleState_4 = 0;
+              Serial.println("Device4 ON");
+              }
+             else{
+              digitalWrite(RelayPin4, HIGH); // turn off relay 4
+              toggleState_4 = 1;
+              Serial.println("Device4 OFF");
+              }
+             delay(100);
+      break;
+      default : break;      
+      }
+  
+}
+
+void with_internet(){
+    //Manual Switch Control
+    if (digitalRead(SwitchPin1) == LOW){
+      delay(200);
+      relayOnOff(1); 
+      Blynk.virtualWrite(VPIN_BUTTON_1, toggleState_1);   // Update Button Widget  
+    }
+    else if (digitalRead(SwitchPin2) == LOW){
+      delay(200);
+      relayOnOff(2);      
+      Blynk.virtualWrite(VPIN_BUTTON_2, toggleState_2);   // Update Button Widget
+    }
+    else if (digitalRead(SwitchPin3) == LOW){
+      delay(200);
+      relayOnOff(3);
+      Blynk.virtualWrite(VPIN_BUTTON_3, toggleState_3);   // Update Button Widget
+    }
+    else if (digitalRead(SwitchPin4) == LOW){
+      delay(200);
+      relayOnOff(4);
+      Blynk.virtualWrite(VPIN_BUTTON_4, toggleState_4);   // Update Button Widget
+    }
+}
+void without_internet(){
+    //Manual Switch Control
+    if (digitalRead(SwitchPin1) == LOW){
+      delay(200);
+      relayOnOff(1);      
+    }
+    else if (digitalRead(SwitchPin2) == LOW){
+      delay(200);
+      relayOnOff(2);
+    }
+    else if (digitalRead(SwitchPin3) == LOW){
+      delay(200);
+      relayOnOff(3);
+    }
+    else if (digitalRead(SwitchPin4) == LOW){
+      delay(200);
+      relayOnOff(4);
+    }
+}
+
+BLYNK_CONNECTED() {
+  // Request the latest state from the server
+  Blynk.syncVirtual(VPIN_BUTTON_1);
+  Blynk.syncVirtual(VPIN_BUTTON_2);
+  Blynk.syncVirtual(VPIN_BUTTON_3);
+  Blynk.syncVirtual(VPIN_BUTTON_4);
+}
+
+// When App button is pushed - switch the state
+
+BLYNK_WRITE(VPIN_BUTTON_1) {
+  toggleState_1 = param.asInt();
+  digitalWrite(RelayPin1, toggleState_1);
+}
+
+BLYNK_WRITE(VPIN_BUTTON_2) {
+  toggleState_2 = param.asInt();
+  digitalWrite(RelayPin2, toggleState_2);
+}
+
+BLYNK_WRITE(VPIN_BUTTON_3) {
+  toggleState_3 = param.asInt();
+  digitalWrite(RelayPin3, toggleState_3);
+}
+
+BLYNK_WRITE(VPIN_BUTTON_4) {
+  toggleState_4 = param.asInt();
+  digitalWrite(RelayPin4, toggleState_4);
+}
+
+
+void checkBlynkStatus() { // called every 3 seconds by SimpleTimer
+
+  bool isconnected = Blynk.connected();
+  if (isconnected == false) {
+    wifiFlag = 1;
+    digitalWrite(wifiLed, LOW); //Turn OFF WiFi LED
+  }
+  if (isconnected == true) {
+    wifiFlag = 0;
+    digitalWrite(wifiLed, HIGH); //Turn ON WiFi LED
+  }
+}
+void setup()
+{
+  Serial.begin(9600);
+
+  pinMode(RelayPin1, OUTPUT);
+  pinMode(RelayPin2, OUTPUT);
+  pinMode(RelayPin3, OUTPUT);
+  pinMode(RelayPin4, OUTPUT);
+
+  pinMode(wifiLed, OUTPUT);
+
+  pinMode(SwitchPin1, INPUT_PULLUP);
+  pinMode(SwitchPin2, INPUT_PULLUP);
+  pinMode(SwitchPin3, INPUT_PULLUP);
+  pinMode(SwitchPin4, INPUT_PULLUP);
+
+  //During Starting all Relays should TURN OFF
+  digitalWrite(RelayPin1, toggleState_1);
+  digitalWrite(RelayPin2, toggleState_2);
+  digitalWrite(RelayPin3, toggleState_3);
+  digitalWrite(RelayPin4, toggleState_4);
+
+  WiFi.begin(WIFI_SSID, WIFI_PASS);
+  timer.setInterval(3000L, checkBlynkStatus); // check if Blynk server is connected every 3 seconds
+  Blynk.config(AUTH);
+}
+
+void loop()
+{  
+  if (WiFi.status() != WL_CONNECTED)
+  {
+    Serial.println("WiFi Not Connected");
+  }
+  else
+  {
+    Serial.println("WiFi Connected");
+    Blynk.run();
+  }
+
+  timer.run(); // Initiates SimpleTimer
+  if (wifiFlag == 0)
+    with_internet();
+  else
+    without_internet();
+}
